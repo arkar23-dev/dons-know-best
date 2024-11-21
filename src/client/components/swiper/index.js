b6519ccb1804cf1af33fff08bcc8a13ada8b71e8
arkar20 <arkar20011@gmail.com>
2024-11-20 16:42:24 -0800

    fixed height issue

--- src/client/components/swiper/index.js
+++ src/client/components/swiper/index.js
@@ -115,9 +115,9 @@ function SwiperItem({ children, loading, ...props }) {
   return (
     <div className={swiperCss.swipeable_item} {...props}>
       {children}
-        <div className={swiperCss.loading_indicator} style={{minHeight: loading?'0px': '20px'}}>
-          <div className={swiperCss.spinner} style={{display: loading ? 'block' :'none'}}></div>
-        </div>
+        {loading && <div className={swiperCss.loading_indicator}>
+          <div className={swiperCss.spinner}></div>
+        </div>}
     </div>
   );
 }
